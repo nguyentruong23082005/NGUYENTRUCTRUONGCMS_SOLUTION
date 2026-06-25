bf55aa4e31f4e0ff7265907f40ec80eb036adb95
tructruong2308 <nguyentruong23082005@gmail.com>
2026-06-25 13:47:47 +0700

    refactor: import and use addressMapper utility in useProvinces hook

--- CMS.Frontend/src/hooks/useProvinces.js
+++ CMS.Frontend/src/hooks/useProvinces.js
@@ -1,4 +1,5 @@
 import { useCallback, useEffect, useState } from 'react';
+import { mapOldAdministrativeNames } from '../utils/addressMapper';
 
 const API_BASE = 'https://provinces.open-api.vn/api';
 const STATIC_BASE = '/data';
@@ -42,43 +43,6 @@ async function loadWards(districtCode) {
     wardCache[districtCode] = list;
     return list;
   }
-}
-
-// Bộ từ điển ánh xạ các đơn vị hành chính cũ trước sáp nhập
-const LEGACY_DISTRICT_MAP = {
-  'quận 2': 'Thành phố Thủ Đức',
-  'quận 9': 'Thành phố Thủ Đức',
-  'quận thủ đức': 'Thành phố Thủ Đức'
-};
-
-const LEGACY_WARD_MAP = {
-  'phường bình an': 'Phường An Khánh',
-  'phường bình khánh': 'Phường An Khánh',
-  'phường an khánh': 'Phường Thủ Thiêm' // Phường An Khánh cũ sáp nhập vào Thủ Thiêm mới
-};
-
-/**
- * Ánh xạ các đơn vị hành chính cũ trước sáp nhập sang đơn vị mới sau sáp nhập
- */
-function mapOldAdministrativeNames(districtName, wardName) {
-  let mappedDistrict = districtName;
-  let mappedWard = wardName;
-
-  if (districtName) {
-    const key = districtName.trim().toLowerCase();
-    if (LEGACY_DISTRICT_MAP[key]) {
-      mappedDistrict = LEGACY_DISTRICT_MAP[key];
-    }
-  }
-
-  if (wardName) {
-    const key = wardName.trim().toLowerCase();
-    if (LEGACY_WARD_MAP[key]) {
-      mappedWard = LEGACY_WARD_MAP[key];
-    }
-  }
-
-  return { districtName: mappedDistrict, wardName: mappedWard };
 }
 
 /**
